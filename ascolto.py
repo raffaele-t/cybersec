cd0662e0e1895ff766f5064c2a42eab11e6330d4
raffaele-t <teseiraffaele@gmail.com>
2026-02-02 10:53:21 +0100

    Create ascolto.py

---
+++ ascolto.py
@@ -0,0 +1,34 @@
+import socket
+server_socket = socket.socket()
+    host = '127.0.0.1'
+    port = 6364
+    server_socket.bind((host, port))
+    server_socket.listen(1)
+    print(f"Server listening on {host} : {port}")
+    conn, addr = server_socket.accept()
+    print(f"Connected by {addr}")
+    #apertura file in scrittura
+    f=open('logfile.txt', "w")
+    f.write(f"Server listening on {host}:{port}\n")
+    for i in range(6):
+        conn, addr = server_socket.accept()
+        f.write(f"Connected ny {addr}\n")
+        try:
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                richiesta = data.decode()
+                if richiesta == 'SHUTDOWN':
+                    break
+                risposta = f"Ho ricevuto il tuo messaggio: {richiesta}\n"
+                #print(risposta)
+                f.write(risposta)
+                conn.sendall(risposta.encode())
+            if richiesta == 'SHUTDOWN':
+                break
+        finally:
+            #chiudere file
+            conn.close()
+    f.close()
+    server_socket.close()
