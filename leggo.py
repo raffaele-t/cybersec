988614bef17d580a21d9169eafb9c4ea394d435a
raffaele-t <teseiraffaele@gmail.com>
2026-02-02 10:17:09 +0100

    Update leggo.py

--- leggo.py
+++ leggo.py
@@ -1,8 +1,8 @@
-nnomefile = "logfile.txt"
-    try:
-        f=open(nomefile, "r", encoding='utf-8')
-        righe=f.readlines()
-        for riga in righe:
-            print(riga, end='')
-    except FileNotFoundError as e:
-        print(f"[-] errore bloccante: {str(e)}")
+nomefile = "logfile.txt"
+try:
+    f=open(nomefile, "r", encoding='utf-8')
+    righe=f.readlines()
+    for riga in righe:
+        print(riga, end='')
+except FileNotFoundError as e:
+    print(f"[-] errore bloccante: {str(e)}")
